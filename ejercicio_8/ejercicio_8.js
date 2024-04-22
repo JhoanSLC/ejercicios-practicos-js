858187d2070f9f577dabdc11913a0f16df1dfa96
JhoanSLC <JhoanSLC4@gmail.com>
2024-04-22 17:50:17 -0500

    feat: :sparkles: Ejercicio 8 terminado :sparkles:
    Ejercicio 8 terminado y funcionando con las respectivas
    pruebas realizadas

---
+++ ejercicio_8/ejercicio_8.js
@@ -0,0 +1,33 @@
+/* 
+
+8. Número de aes (letra "a")
+
+Escribir una función llamada numeroDeAes que reciba un string y retorne el número de veces que aparece la
+letra "a":
+
+    // código de prueba
+    console.log(numeroDeAes("abracadabra")) // 5
+    console.log(numeroDeAes("etinol")) // 0
+    console.log(numeroDeAes("")) // 0
+
+*/
+
+const numeroDeAes = (string) => {
+    // Validación de que el tipo de dato ingresado sea un string
+    if (typeof(string) !== 'string') {console.log('El valor ingresado debe ser un string'); return;}
+
+    // Crea contador de 'a' dentro del string
+    let aCounter = 0;
+
+    // Recorre cada sub-string del string 
+    for (let char of string) {
+        if (char === 'a') { // Si el sub-string es una 'a'
+            aCounter += 1;  // entonces suma en 1 al contador de 'a'
+        }
+    }
+
+    return aCounter;
+
+}
+
+console.log(numeroDeAes(''))
